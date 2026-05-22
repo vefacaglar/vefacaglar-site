00a3ce96c5ea53babfcf7c1151df4c6795693de5
Vefa Çağlar Özcan <vefacaglarozcan@gmail.com>
2026-05-22 13:06:28 +0300

    rerun project after changes

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -1,3 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  webpack: (config, { dev, isServer }) => {
+    if (dev && !isServer) {
+      config.watchOptions = {
+        poll: 1000,
+        aggregateTimeout: 300,
+        ignored: /node_modules/,
+      };
+    }
+    return config;
+  },
+};
 module.exports = nextConfig;
